df706fd6ea32e630c591ac2d13d38d367ceeabe9
Edouard Rolland <edr@mmmi.sdu.dk>
2026-06-19 00:10:00 +0200

    [Bug Fix] M400 - PORT PAYLOAD

--- WildBridgeApp/android-sdk-v5-sample/src/main/java/dji/sampleV5/aircraft/DroneControlProfiles.kt
+++ WildBridgeApp/android-sdk-v5-sample/src/main/java/dji/sampleV5/aircraft/DroneControlProfiles.kt
@@ -82,7 +82,7 @@ enum class DroneControlProfile(
         yawKp = 3.0,
         maxYawRateDegS = 30.0,
         defaultCruiseSpeedMps = 25.0,
-        payloadIndexType = PayloadIndexType.PORT_1,
+        payloadIndexType = PayloadIndexType.PORT_3,
         dropArmSwitchIndex = 3,
         dropReleaseButtonIndex = 5
     ),
